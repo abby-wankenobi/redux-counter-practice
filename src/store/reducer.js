12c0cf1ca3f7dab14a378c49fb034f173bd1bfca
Abigail Swarth <aswarth@NY-057.thecementworks.com>
2019-08-12 17:55:45 -0400

    changing state immutably

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -7,19 +7,29 @@ const reducer = (state = initialState, action) => {
   switch(action.type){
     case 'INCREMENT':
       return {
+        ...state,
         counter: state.counter + 1
       }
     case 'DECREMENT':
       return {
+        ...state,
         counter: state.counter - 1
       }
     case 'ADD':
       return {
+        ...state,
         counter: state.counter + action.payload
       }
     case 'SUBTRACT':
       return {
+        ...state,
         counter: state.counter - action.payload
+      }
+    case 'STORE_RESULT':
+      return {
+        ...state,
+        //uses concat in order to update state immutably
+        results: state.results.concat({id: new Date(), value: state.counter})
       }
   }
   return state;
